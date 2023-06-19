6f66f91589f34148344c1e0d4d99449c1ce2c3b1
Pd Rajan <95139297+PdRajan@users.noreply.github.com>
2023-06-19 14:27:04 +0530

    Update Coordinates.java

--- com/PDFtoCSV/Coordinates.java
+++ com/PDFtoCSV/Coordinates.java
@@ -5,132 +5,132 @@ public final class Coordinates
 {
     // top header
 
-    public static final float DLLY = 0.0f;
-    public static final float DURY = 90.0f;
-    public static final float DLLX = 750.0f;
-    public static final float DURX = 1180.0f;
-
     // public static final float DLLY = 0.0f;
-    // public static final float DURY = 100.0f;
-    // public static final float DLLX = 600.0f;
-    // public static final float DURX = 1172.0f;
+    // public static final float DURY = 90.0f;
+    // public static final float DLLX = 750.0f;
+    // public static final float DURX = 1180.0f;
+
+    public static final float DLLY = 0.0f;
+    public static final float DURY = 100.0f;
+    public static final float DLLX = 600.0f;
+    public static final float DURX = 1172.0f;
     //************************************************* */
 
 
     // Scheme of exam
-    public static final float SOELLY = 100.0f;
-    public static final float SOELLX = 450.0f;
-    public static final float SOEURY = 124.0f;
-    public static final float SOEURX = 740.0f;
+    // public static final float SOELLY = 100.0f;
+    // public static final float SOELLX = 450.0f;
+    // public static final float SOEURY = 124.0f;
+    // public static final float SOEURX = 740.0f;
 
-    // public static final float SOELLY = 110.0f;
-    // public static final float SOELLX = 455.0f;
-    // public static final float SOEURY = 130.0f;
-    // public static final float SOEURX = 690.0f;
+    public static final float SOELLY = 110.0f;
+    public static final float SOELLX = 455.0f;
+    public static final float SOEURY = 130.0f;
+    public static final float SOEURX = 690.0f;
     //********************************************* */
 
     // header 
 
-    public static final float PHLLY = 167.0f;
-    public static final float PHLLX = 26.0f;
-    public static final float PHURY = 197.0f;
-    public static final float PHURX = 1100.0f;
-
-    // public static final float PHLLY = 100.0f;
-    // public static final float PHLLX = 30.0f;
-    // public static final float PHURY = 125.0f;
+    // public static final float PHLLY = 167.0f;
+    // public static final float PHLLX = 26.0f;
+    // public static final float PHURY = 197.0f;
     // public static final float PHURX = 1100.0f;
 
-    // header of Scheme of exam
-    public static final float PDLLY = 155.0f;
-    public static final float PDLLX = 0.0f;
-    public static final float PDURY = 180.0f;
-    public static final float PDURX = 1200.0f;
+    public static final float PHLLY = 100.0f;
+    public static final float PHLLX = 30.0f;
+    public static final float PHURY = 125.0f;
+    public static final float PHURX = 1100.0f;
 
-    // public static final float PDLLY = 140.0f;
-    // public static final float PDLLX = 60.0f;
-    // public static final float PDURY = 175.0f;
-    // public static final float PDURX = 900.0f;
+    // header of Scheme of exam
+    // public static final float PDLLY = 155.0f;
+    // public static final float PDLLX = 0.0f;
+    // public static final float PDURY = 180.0f;
+    // public static final float PDURX = 1200.0f;
+
+    public static final float PDLLY = 140.0f;
+    public static final float PDLLX = 60.0f;
+    public static final float PDURY = 175.0f;
+    public static final float PDURX = 900.0f;
 
     // Enrollment number from result data
-    public static final float SRNLLY = 200.0f;
-    public static final float SDLLX = 85.0f;
-    public static final float SRNURY = 207.0f;
-    public static final float SDURX = 226.0f;
+    // public static final float SRNLLY = 200.0f;
+    // public static final float SDLLX = 85.0f;
+    // public static final float SRNURY = 207.0f;
+    // public static final float SDURX = 226.0f;
 
-    // public static final float SRNLLY = 125.0f;
-    // public static final float SDLLX = 86.0f;
-    // public static final float SRNURY = 135.0f;
-    // public static final float SDURX = 245.0f;
+    public static final float SRNLLY = 125.0f;
+    public static final float SDLLX = 86.0f;
+    public static final float SRNURY = 135.0f;
+    public static final float SDURX = 245.0f;
     
     //***********************************************************************************************************************************/
 
     // student name lower and uppper in result
 
-    public static final float SNLLY = 208.0f;
-    public static final float SNURY = 217.0f;
+    // public static final float SNLLY = 208.0f;
+    // public static final float SNURY = 217.0f;
 
-    // public static final float SNLLY = 134.0f;
-    // public static final float SNURY = 143.0f;
+    public static final float SNLLY = 134.0f;
+    public static final float SNURY = 143.0f;
     //******************************************* */
     
     // student id or SID on result page
     
-    public static final float SIDLLY = 218.0f;
-    public static final float SIDURY = 226.0f;
+    // public static final float SIDLLY = 218.0f;
+    // public static final float SIDURY = 226.0f;
     
-    // public static final float SIDLLY = 143.0f;
-    // public static final float SIDURY = 153.0f;
+    public static final float SIDLLY = 143.0f;
+    public static final float SIDURY = 153.0f;
     
     //******************************************* */
 
     // student Scheme id or SSID on result page
 
-    public static final float SSIDLLY = 227.0f;
-    public static final float SSIDURY = 235.0f;
-    // public static final float SSIDLLY = 153.0f;
-    // public static final float SSIDURY = 162.0f;
+    // public static final float SSIDLLY = 227.0f;
+    // public static final float SSIDURY = 235.0f;
+    public static final float SSIDLLY = 153.0f;
+    public static final float SSIDURY = 162.0f;
 
     // ********************************************
 
     // Student detail interval in Y
 
-    public static final float SDIY = 54.0f; 
-    // public static final float SDIY = 67.0f; 
+    // public static final float SDIY = 54.0f; 
+    public static final float SDIY = 67.0f; 
     // *************************************************
 
     // BBA(BI) 301 (4)
 
-    public static final float MLLY = 197.0f;
-    public static final float MLLX = 226.8f;
-    public static final float MURY = 216.0f;
-    public static final float MURX = 270.72f;
+    // public static final float MLLY = 197.0f;
+    // public static final float MLLX = 226.8f;
+    // public static final float MURY = 216.0f;
+    // public static final float MURX = 270.72f;
 
-    // public static final float MLLY = 125.0f;
-    // public static final float MLLX = 246.0f;
-    // public static final float MURY = 145.0f;
-    // public static final float MURX = 292.0f;
+    public static final float MLLY = 125.0f;
+    public static final float MLLX = 246.0f;
+    public static final float MURY = 145.0f;
+    public static final float MURX = 292.0f;
 
     // *****************************************************************
 
-    public static final float MRI = 18.0f;
-    public static final float MRSI = 43.92f;
-    public static final float MIEI = 22.32f;
+    // public static final float MRI = 18.0f;
+    // public static final float MRSI = 43.92f;
+    // public static final float MIEI = 22.32f;
 
-    // public static final float MRI = 22.3f;
-    // public static final float MRSI = 46.00f;
-    // public static final float MIEI = 23.00f;
+    public static final float MRI = 22.3f;
+    public static final float MRSI = 46.00f;
+    public static final float MIEI = 23.00f;
 
     //****************************************************************/
 
     // CS / Remark
 
-    public static final float CLLY = 198.0f;
-    public static final float CLLX = 1102.0f;
-    public static final float CURY = 252.0f;
-    public static final float CURX = 1185.0f;
-    // public static final float CLLY = 125.0f;
-    // public static final float CLLX = 1028.0f;
-    // public static final float CURY = 191.0f;
-    // public static final float CURX = 1152.0f;
+    // public static final float CLLY = 198.0f;
+    // public static final float CLLX = 1102.0f;
+    // public static final float CURY = 252.0f;
+    // public static final float CURX = 1185.0f;
+    public static final float CLLY = 125.0f;
+    public static final float CLLX = 1028.0f;
+    public static final float CURY = 191.0f;
+    public static final float CURX = 1152.0f;
 }
